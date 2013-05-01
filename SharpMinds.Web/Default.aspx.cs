e97611642384549076d28707ba8c5465760f54d4
Carsten Nilsson <cani@sharpminds.dk>
2013-05-02 00:12:23 +0200

    Trying to fix jQeury

--- SharpMinds.Web/Default.aspx.cs
+++ SharpMinds.Web/Default.aspx.cs
@@ -9,9 +9,6 @@ namespace SharpMinds.Web
 {
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
+     
     }
 }
--- SharpMinds.Web/Scripts/SharpMinds.js
+++ SharpMinds.Web/Scripts/SharpMinds.js
@@ -1,1 +1,17 @@
 ﻿// SharpMinds - JavaScript library, all rights reserved
+
+var browser = navigator.appName;
+if (browse.contains('Microsoft')) {
+    $window.getElementById('alert_button').hide();
+}
+
+function detectBrowser() {
+    if (browser == "Microsoft Internet Explorer") {
+        document.write("IE sux");
+        window.alert(browser);
+    }
+}
+
+function alert() {
+    alert('hello, world!');
+}
