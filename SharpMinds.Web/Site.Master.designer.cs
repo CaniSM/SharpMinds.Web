9dedf464502707567748e4af98bc4a10d3d25f45
Carsten Nilsson <cani@sharpminds.dk>
2013-05-01 17:07:30 +0200

    Changes

--- SharpMinds.Web/Site.Master.designer.cs
+++ SharpMinds.Web/Site.Master.designer.cs
@@ -49,15 +49,6 @@ namespace SharpMinds.Web {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor A3;
         
         /// <summary>
-        /// LoginView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginView LoginView1;
-        
-        /// <summary>
         /// FeaturedContent control.
         /// </summary>
         /// <remarks>
